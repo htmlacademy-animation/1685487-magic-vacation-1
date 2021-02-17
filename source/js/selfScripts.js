f743f1cb7215869abb98c73ecd004693b457c2d7
Nadezhda <svobodinang@gmail.com>
2021-02-17 22:17:20 +0300

    from string to span func

--- source/js/selfScripts.js
+++ source/js/selfScripts.js
@@ -5,6 +5,21 @@ export default () => {
     body.classList.add('onload')
     window.removeEventListener("load", loadBodyHandler, false);
   }
-
   window.addEventListener("load", loadBodyHandler, false);
+
+  // ! module 2
+  function animateText(domEl, animationSettings = {}) {
+    let text = domEl.innerHTML
+    domEl.innerHTML = ''
+    let stringSpan = document.createElement('span')
+    for (let letter of text.split('')) {
+      let letterSpan = document.createElement('span')
+      letterSpan.textContent = letter
+      stringSpan.appendChild(letterSpan)
+    }
+    domEl.appendChild(stringSpan)
+  }
+
+  let mainTitle = document.getElementById('mainTitle')
+  animateText(mainTitle)
 }
